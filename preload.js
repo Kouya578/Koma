d8b087584ff57d5bc7f520c4438ee14e193df55f
Koya <71790791+Kouya578@users.noreply.github.com>
2024-08-28 21:43:56 +0700

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+})
+
+/* I not more in for in test
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
+})
+*/
